fb2c602d22eb810225824712c04806ec4985ff8b
moazzamali216 <113228966+moazzamali216@users.noreply.github.com>
2024-12-01 12:56:34 +0500

    main

--- src/components/Cloud/Hero.jsx
+++ src/components/Cloud/Hero.jsx
@@ -4,8 +4,8 @@ import hero from "../../assets/images/hero-img.png"; // Make sure the path is co
 const Hero = () => {
   return (
     <>
-<div className=" container pb-10  mx-auto ">
-<div className="overflow-hidden container md:flex-row  gap-x-4 flex-col px-4 xl:px-[120px] mx-auto w-full flex md:pt-[140px] pt-[120px] md:h-[500px] lg:h-[700px]">
+<div className=" container   mx-auto ">
+<div className="overflow-hidden container md:flex-row  gap-x-4 flex-col px-4 xl:px-[120px] mx-auto w-full flex md:pt-[140px] pt-[120px] md:h-[600px] lg:h-[700px]">
 
  <div className="h-full flex flex-col  lg:pr-8 xl:pr-16   justify-center  w-full md:w-1/2">
  <h1  className="  leading-[50px] text-[#3C3C3C] lg:leading-[60px] text-[35px] lg:text-[50px] mt-5 font-poppins font-[425] ">Efficiently scale cloud experiences everywhere.</h1>
